882cc7e8bf5e56f025de9de34521a7d4e10e123f
ravikiran arnepalli <ravikiran.arnepalli@lifexinfo.com>
2021-11-11 16:56:00 +0530

    make it better

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ io.on("connection", (socket) => {
 
   // Listen to chantMessage event sent by client and emit a chatMessage to the client
   socket.on("chat message", function (message) {
+    io.emit("Serverlog", "message.receiver is not available");
     if(message.receiver !=""|| message.receiver !=undefined || message.receiver !=null){
       io.to(message.receiver).emit("chat message", message);
     }else{
